09072037e7dd7e176d5b00a86571002468616ae6
ron <lucifron28@gmail.com>
2025-10-16 15:29:25 +0800

    Add MarkdownPipe to transform markdown text to HTML

---
+++ src/app/shared/pipes/markdown.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'markdown',
+  standalone: true
+})
+export class MarkdownPipe implements PipeTransform {
+  transform(value: string): string {
+    // TODO: Transform markdown to HTML using ngx-markdown
+    return value;
+  }
+}
