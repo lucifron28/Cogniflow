4c90f828d221e218597b874fc405aa80304f4e4b
ron <lucifron28@gmail.com>
2025-10-16 15:29:09 +0800

    Add ThemeToggleComponent with HTML template and initial structure

---
+++ src/app/shared/components/theme-toggle/theme-toggle.component.ts
@@ -0,0 +1,13 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ThemeService } from '../../../core/services/theme.service';
+
+@Component({
+  selector: 'app-theme-toggle',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './theme-toggle.component.html'
+})
+export class ThemeToggleComponent {
+  themeService = inject(ThemeService);
+}
